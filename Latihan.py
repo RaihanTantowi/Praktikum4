a37bcfe5aae7d27dff46e3d987d1d6e594b8d74c
RaihanTantowi <raihantantowi9@gmail.com>
2021-12-03 20:04:15 +0700

    Pernambahan file

--- Latihan.py
+++ Latihan.py
@@ -1,18 +1,18 @@
 #List dengan 5 elemen
-print("="*35)
+print("="*73)
 nilai = [11, 12, 13, 14, 15]
 print("Membuat sebuah list dengan 5 elemen: ")
 print(nilai[0:6])
-print("="*35)
+print("="*73)
 
 #Akses pada list
 print("Akses list")
 print ("element ke 3 : ", nilai[2])
 print ("element ke 2 sampai ke 4 : ", nilai[1:4])
-print("="*35)
+print("="*73)
 
 #mengubah element list
-print("Mengubah elemen list")
+print("Ubah elemen list")
 odd = [11,12,13,14,15]
 odd[3] = 10
 print("Mengubah elemen ke 4 dengan nilai lainnya: ", odd)
@@ -20,10 +20,10 @@ print("Mengubah elemen ke 4 dengan nilai lainnya: ", odd)
 #Mengubah elemen ke 4 sampai dengan elemen terakhir
 odd[3:5] = [16,18]
 print("Mengubah elemen ke 4 sampai dengan elemen terakhir: ",odd)
-print("="*30)
+print("="*73)
 
 #menambahkan element
-print("Menambahkan element")
+print("Tambah element list")
 nilaia = [11,22,14]
 nilaib = [24,12,52]
 print("list a : ", nilaia)
@@ -33,6 +33,6 @@ print ("menambahkan list B nilai string : ", nilaib)
 nilaib.extend([60,70,80])
 print ("menambahkan list B dengan 3 nilai", nilaib)
 print ("mengabungkan list A dan B : " ,nilaia + nilaib)
-print("="*35)
+print("="*73)
 print("Thanks :)")
-print("="*35)
+print("="*73)
